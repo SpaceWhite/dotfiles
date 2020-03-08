75d53c37a47fa8470725015bf0fd4ec23cf37ac6
SpaceWhite <space_white@yahoo.com>
2020-03-08 07:34:07 -0800

    add ttyd service

--- sec/ttyd.sh
+++ sec/ttyd.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 wget https://github.com/tsl0922/ttyd/releases/download/1.6.0/ttyd_linux.x86_64
-mkdir $TOOLDIR/ttyd
+mkdir -p $TOOLDIR/ttyd
 mv ttyd_linux.x86_64 $TOOLDIR/ttyd/ttyd
 chmod +x $TOOLDIR/ttyd/ttyd
+
+mkdir -p $TOOLDIR/services/
+
+echo "$TOOLDIR/ttyd/ttyd -c admin:Admin123456789 bash &>/dev/null &" > $TOOLDIR/services/ttyd.sh
+echo "disown" >> $TOOLDIR/services/ttyd.sh
+
+chmod +x $TOOLDIR/services/ttyd.sh
